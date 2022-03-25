9cc6c08e7f5a7760fe4a89df74b2d4c211f1a349
Raman <rs.kala@hotmail.com>
2022-03-25 02:56:16 -0400

    Complete general info section
    Completed the form for the general info section.  Need to try and
    implement it as a different module to clean up App.js.

--- src/components/GeneralInfo.js
+++ src/components/GeneralInfo.js
@@ -1,36 +1,8 @@
-import React, { Component } from 'react';
 
-class App extends Component {
-  constructor () {
-    super();
+const GeneralInfo = (props) => {
 
-    this.state = {
-      name : 'Enter full name',
-      email : 'Enter e-mail',
-      phoneNo: '(xxx)-xxx-xxxx',
-    };
-  }
-  handleNameChange = (e) => {
-    this.setState({
-      name : e.target.value,
-    });
-  }
-
-  handleEmailChange = (e) => {
-    this.setState({
-      email: e.target.value,
-    });
-  }
-
-  handleNumberChange = (e) => {
-    this.setState({
-      phoneNo: e.target.value,
-    });
-  }
-
-  render() {
     return (
-      <div>
+        <div>
         <form>
           <h2>General Info</h2>
           <label htmlFor='nameInput'>Name:</label>
@@ -43,8 +15,6 @@ class App extends Component {
         </form>
       </div>
     )
-  }
-
 }
 
-export default App;
+export default GeneralInfo;
