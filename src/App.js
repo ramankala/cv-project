817e30a104e70f75b8744dc1d6019c74e41fa034
Raman <rs.kala@hotmail.com>
2022-03-29 01:44:24 -0400

    Add onChange logic for Work Experience section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import GeneralInfo from './components/GeneralInfo';
 import EducationInfo from './components/EducationInfo';
+import WorkExperience from './components/WorkExperience';
 class App extends Component {
   constructor () {
     super();
@@ -11,6 +12,9 @@ class App extends Component {
       phoneNo: '(xxx)-xxx-xxxx',
       schoolName: 'Enter name of school',
       studyTitle: 'Enter Degree/Field of Study',
+      companyName: 'Enter name of Company',
+      positionTitle: 'Enter Job Title',
+      workDesc: 'Enter main role in this position',
     };
   }
   handleNameChange = (e) => {
@@ -38,9 +42,33 @@ class App extends Component {
       studyTitle: e.target.value,
     });
   }
+  handleCompanyName = (e) => {
+    this.setState({
+      companyName: e.target.value,
+    });
+  }
+  handlePositionTitle = (e) => {
+    this.setState({
+      positionTitle: e.target.value,
+    });
+  }
+  handleWorkDesc = (e) => {
+    this.setState({
+      workDesc: e.target.value,
+    });
+  }
 
   render() {
-    const { name, email, phoneNo, schoolName, studyTitle } = this.state;
+    const { 
+      name,
+      email, 
+      phoneNo, 
+      schoolName, 
+      studyTitle,
+      companyName,
+      positionTitle,
+      workDesc,
+      } = this.state;
     return (
       <div>
         <GeneralInfo 
@@ -57,6 +85,14 @@ class App extends Component {
           handleSchool={this.handleSchoolChange.bind(this)}
           handleTitle={this.handleTitleChange.bind(this)}
           />
+        <WorkExperience
+        companyName={companyName}
+        positionTitle={positionTitle}
+        workDesc={workDesc}
+        handleCompanyName={this.handleCompanyName.bind(this)}
+        handlePositionTitle={this.handlePositionTitle.bind(this)}
+        handleWorkDesc={this.handleWorkDesc.bind(this)}
+         />
       </div>
     )
   }
---
+++ src/components/WorkExperience.js
@@ -0,0 +1,28 @@
+const WorkExperience = (props) => {
+    const { 
+        companyName,
+        positionTitle, 
+        workDesc, 
+        handleCompanyName, 
+        handlePositionTitle,
+        handleWorkDesc} = props;
+    return (
+        <div>
+            <form>
+                <h2>Work Experience</h2>
+                <label htmlFor="companyName">Company Name: </label>
+                <input onChange={handleCompanyName} type="text" id="companyName" placeholder={companyName} />
+                <label htmlFor="positionTitle">Position Title: </label>
+                <input onChange={handlePositionTitle} type="text" id="positionTitle" placeholder={positionTitle} />
+                <label htmlFor="workDesc">Work Description: </label>
+                <input onChange={handleWorkDesc} type="text" id="workDesc" placeholder={workDesc} />
+                <label htmlFor="workFrom">Start Date: </label>
+                <input type="date" id="workFrom" />
+                <label htmlFor="workTo">End Date:</label>
+                <input type="date" id="workTo" />
+            </form>
+        </div>
+    )
+}
+
+export default WorkExperience;
